8d21da6afeb0f6beb18f32ef49fe0c4f857c770e
Aleksandr Romanov <Aleksandr.Romanov@kelnik.ru>
2017-09-20 14:29:32 +0300

    video

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -7,31 +7,29 @@ const _ = require('lodash');
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.option('ns', {type: String, description: 'namespace of entity', required: false, alias: 'n', default:''});
+    this.option('ns', {type: String, description: 'namespace of entity', required: false, alias: 'n', default: ''});
     this.option('cwd', {type: String, description: 'directory where create entities', required: false, default: ''});
     this.option('showGreeting', {type: Boolean, description: 'show greeting', required: false, default: true, hide: true});
-    this.option('entity', {type: String, description: 'comma separated list of entities', required: false, optional: true, hide: true, default:''});
+    this.option('entity', {type: String, description: 'comma separated list of entities', required: false, optional: true, hide: true, default: ''});
     this.option('showNs', {type: String, description: 'show namespace question', required: false, optional: true, hide: true, default: true});
     this.option('showEntities', {type: String, description: 'show entities question', required: false, optional: true, hide: true, default: true});
 
-    this.argument('entity', {type: String, description: 'comma separated list of entities', required:false, optional: true});
-
+    this.argument('entity', {type: String, description: 'comma separated list of entities', required: false, optional: true});
 
     if (this.options.cwd.length > 0 && this.options.cwd[this.options.cwd.length - 1] !== '/') {
       this.options.cwd += '/';
     }
     this.props = {
-        entity: this.options.entity,
-        namespace: this.options.ns,
+      entity: this.options.entity,
+      namespace: this.options.ns
 
     };
-    //this.log(this.options.cwd);
+    // This.log(this.options.cwd);
   }
 
-
   prompting() {
-    if (this.options.showGreeting){
-        this.log(yosay(
+    if (this.options.showGreeting) {
+      this.log(yosay(
             'Welcome to the great ' + chalk.red('generator-bitrix') + ' generator!'
         ));
     }
@@ -43,48 +41,48 @@ module.exports = class extends Generator {
       name: 'entity',
       message: 'Enter entities, that you want to create (comma separated):',
       default: this.options.entity,
-        when: function (answers) {
-            return !self.options.showEntities;
-        }
+      when: function (answers) {
+        return self.options.showEntities;
+      }
     });
     prompts.push({
       type: 'input',
       name: 'namespace',
       message: 'Base namespace of entities: ',
       default: this.options.ns,
-        when: function(answers){
-          return self.options.ns && !self.options.showNs;
-        }
+      when: function (answers) {
+        return self.options.ns && self.options.showNs;
+      }
     });
 
     return this.prompt(prompts).then(props => {
       this.props = _.extend(this.props, props);
-      if (this.props.entity.trim().length){
-          this.props.entity = this.props.entity.split(',').map(item => {
-              return {
-                  fileName: item.trim().toLowerCase(),
-                  className: item.trim()
-              };
-          });
+      if (this.props.entity.trim().length) {
+        this.props.entity = this.props.entity.split(',').map(item => {
+          return {
+            fileName: item.trim().toLowerCase(),
+            className: item.trim()
+          };
+        });
 
-          let askInterfaces = [];
-          for (let i = 0; i < this.props.entity.length; ++i) {
-              askInterfaces.push({
-                  type: 'confirm',
-                  name: this.props.entity[i].className,
-                  message: 'Create interface for entity ' + this.props.entity[i].className
-              });
-          }
-          return this.prompt(askInterfaces).then(interfaces => {
-              this.props.interfaces = interfaces;
+        let askInterfaces = [];
+        for (let i = 0; i < this.props.entity.length; ++i) {
+          askInterfaces.push({
+            type: 'confirm',
+            name: this.props.entity[i].className,
+            message: 'Create interface for entity ' + this.props.entity[i].className
           });
+        }
+        return this.prompt(askInterfaces).then(interfaces => {
+          this.props.interfaces = interfaces;
+        });
       }
     });
   }
 
-    paths() {
-        this.destinationRoot(this.options.cwd ? this.options.cwd : '.');
-    }
+  paths() {
+    this.destinationRoot(this.options.cwd ? this.options.cwd : '.');
+  }
 
   writing() {
     let model, tplParams;
@@ -96,7 +94,7 @@ module.exports = class extends Generator {
       };
       this.fs.copyTpl(
             this.templatePath('model/model.php'),
-            this.destinationPath( model.fileName + '/' + model.fileName + '.php'),
+            this.destinationPath(model.fileName + '/' + model.fileName + '.php'),
             tplParams
         );
       if (this.props.interfaces[model.className]) {
@@ -112,7 +110,7 @@ module.exports = class extends Generator {
             );
         this.fs.copyTpl(
                 this.templatePath('admininterface/edithelper.php'),
-                this.destinationPath( model.fileName + '/admininterface/' + model.fileName + 'edithelper.php'),
+                this.destinationPath(model.fileName + '/admininterface/' + model.fileName + 'edithelper.php'),
                 tplParams
             );
         this.fs.copyTpl(
